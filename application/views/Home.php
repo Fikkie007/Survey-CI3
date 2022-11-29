c1ed7ce17399267ff2e481a9272e5f22646e3e57
reyhansyamsudin <resyaa16@gmail.com>
2022-11-29 13:24:36 +0700

    Tampilan New

--- application/views/Home.php
+++ application/views/Home.php
@@ -1,197 +1,192 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<style>
-    .container {
-        background: linear-gradient(180deg, #768480 0%, rgba(118, 132, 128, 0) 143.63%);
-        border-radius: 30px;
-    }
+  <head>
+    <style>
+        *{
+    margin: 0;
+    padding: 0;
+}
 
-    .map-container {
-        overflow: hidden;
-        padding-bottom: 56.25%;
-        position: relative;
-        height: 0;
-    }
+/* navbar */
+#nav{
+    position: sticky;
+    top: 0;
+    z-index: 1001;
+}
 
-    .map-container iframe {
-        left: 0;
-        top: 0;
-        height: 100%;
-        width: 100%;
-        position: absolute;
-    }
+.navbar{
+    background-image: linear-gradient(to right, #d1e3ec, #2f90f1);
 
-    .perumda {
-        font-family: 'Jost';
-        font-style: normal;
-        font-weight: 700;
-        font-size: 48px;
-        line-height: 69px;
-        color: #fff;
+}
 
-    }
+.navbar-brand span{
+    font-weight: bolder;
+    color: #fff;
+}
 
-    .card {
-        position: relative;
-        display: -webkit-box;
-        display: -ms-flexbox;
-        display: flex;
-        -webkit-box-orient: vertical;
-        -webkit-box-direction: normal;
-        -ms-flex-direction: column;
-        flex-direction: column;
-        min-width: 0;
-        word-wrap: break-word;
-        background-color: transparent;
-        background-clip: border-box;
-        border: 1pxsolidrgba(0, 0, 0, .125);
-        border-radius: 0.25rem;
-    }
+.navbar-nav li{
+    padding: 0 10px;
+}
 
-    .line {
-        background: #FFF9F9;
-    }
+.navbar-nav li a{
+    color: #fff !important;
+    font-weight: bold;
+    text-transform: uppercase;
+}
 
-    .a {
-        color: none;
-        text-decoration: none;
-        background-color: transparent;
-    }
-</style>
+/* navbar */
+#banner{
+    position: relative;
+    background-image: linear-gradient(to right, #1ea6ec, #2c92fb);
+    color: #fff;
+    padding-top: 5%;
+}
 
-<head>
+.ban-img{
+    position: relative;
+    z-index: 1000;
+}
+
+.wave{
+    position: absolute;
+    bottom: -10%;
+    left: 0;
+}
+
+/* features */
+#features{
+    padding: 80px 0;
+}
+
+.features{
+    padding: 20px;
+}
+
+.features img{
+    width: 80px;
+    height: 80px;
+    margin-top: 10px;
+}
+
+.features h4{
+    padding: 5px;
+    margin-top: 25px;
+    text-transform: uppercase;
+}
+
+    </style>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>SURVEY</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="shortcut icon" type="image/png" href="Assets/web.png" />
-    <link href='https://fonts.googleapis.com/css?family=Jost' rel='stylesheet'>
+    <link rel="stylesheet" href="assets/css/style.css">
 
-    <nav class="navbar navbar-light justify-content-between" style="background-color:#EDF3F3">
-        <a href="https://perumdatugutirta.co.id/">
-            <img src='Assets/Logo.png' />
-        </a>
-        </button>
-        <a href="<?= base_url('/'); ?>">
-            <button type="button" class="btn btn-primary" style="background-color:#4FADD6">Beranda</button>
-        </a>
-    </nav>
-
-</head>
-
-<body>
-    <br>
-    <div class="container-fluid">
-        <?= $this->session->flashdata('message'); ?>
-        <div class="card" style="background:linear-gradient(135deg, #ABDCFF 0%, #0396FF 100%);
-            border-radius: 48px 48px 300px 48px;">
-            <div class="container-fluid">
-                <div class="card-body">
-                </div>
-                <div class="survey">
-                    <h5 class="card-title" style="font-size: 30px;">SURVEY KEPUASAN PELANGGAN <br>PERUMDA AIR MINUM<br>
-                        KOTA MALANG</h5>
-                </div>
-                <br>
-                <div class="kepuasan">
-                    <p class="card-text">Kepuasan pelanggan adalah kebanggan kami sebagai penyedia layanan <br>perumda air minum.
-                        Dengan semangat baru kami berupaya terus meningkatkan <br> dari pelayanan terbaik untuk masyarakat</p>
-                </div>
-                <br>
-                <div class="mari">
-                    <p class="card-text">
-                        <small>Mari Berpartisipasi Untuk Mengisi Kuesioner <br>Periode Th. 2022 - 2023</small>
-                </div>
-                <br>
-                <div class="container-fluid">
-                    <div class="button">
-                        <a href="/survey" class="btn btn-primary" style="background-color: rgba(240, 237, 237, 0.26);
-                    border-color: none;">
-                            Klik Isi Kuesioner
-                        </a>
-                        <br><br>
-                    </div>
-                </div>
-
+    <title>Tugu Tirta Survey</title>
+    <link rel="website icon" type="png"
+            href="Assets\img\Logo_PerumdaTuguTirta-removebg-preview.png">
+            
+  </head>
+  <body>
+     <!-- Navbar -->
+     <section id="nav">
+        <nav class="navbar navbar-expand-lg navbar-">
+            <div class="container">
+            <img src="Assets\img\logo.png">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Contact</a>
+                </li>
+              </ul>
+              </div>
             </div>
-        </div>
-    </div>
+          </nav>
+     </section>
 
-    </div>
+      <!-- Banner -->
+      <section id="banner">
+        <div class="banner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h4>Survey Kepuasan Pelanggan Perumda Air Minum Kota Malang</h4>
+                        <p>Kepuasan pelanggan adalah kebanggan kami sebagai penyedia layanan perumda air minum. Dengan semangat baru kami berupaya terus meningkatkan kualitas dari pelayanan terbaik untuk masyarakat.</p>
+                        <p>Mari Berpartisipasi Untuk Mengisi Kuesioner</p>
+                        <p>Periode Th. 2022 - 2023</p>
 
-</body><br><br>
-
-<footer>
-    <div class="container-fluid" style="background: linear-gradient(180deg, #768480 0%, rgba(118, 132, 128, 0) 143.63%);
-    border-radius: 30px;">
-        <br>
-
-        <div class="row">
-            <div class="col-sm-6">
-                <div class="card">
-                    <div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 500px">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7902.554253189393!2d112.66576080632933!3d-7.970285437617577!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb9b85e5b6f5d00fe!2sPerumda%20Air%20Minum%20Tugu%20Tirta%20Kota%20Malang!5e0!3m2!1sid!2sid!4v1658896606086!5m2!1sid!2sid" frameborder="0" style="border:0" allowfullscreen></iframe>
+                          <a href="Pertanyaan.html"><button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
+                          Klik Disini </button></a>
                     </div>
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div class="card" style="background-color: transparent; border: none;">
-                    <div class="card-body">
-                        <div class="perumda">
-                            <br>
-                            <h5 class="card-title">PERUMDA TUGU TIRTA<br>KOTA MALANG<br><br></h5>
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <div class="card" style="background-color: transparent; border: none;">
-                                        <div class="card-body">
-                                            <h6 class="card-title">Kantor Pusat :<br><br>
-                                                Jl. Terusan Danau Sentani no.100
-                                                Kota Malang - Jawa Timur Indonesia.</h6>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="card" style="background-color: transparent; border: none;">
-                                        <div class="card-body">
-                                            <h6 class="card-title">Kontak :<br>
-                                                Phone (+62) 0341-715-103<br>
-                                                Fax (+62) 0341-715-107<br>
-                                                Email humas@perumdatugutirta.co.id</h6>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="line">
-                            <hr>
-                        </div>
-                        <div class="container-fluid">
-                            <a href="https://www.instagram.com/perumdatugutirta/" style="color: black;">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
-                                    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
-                                </svg>
-                            </a>
-                            <a href="https://id-id.facebook.com/pdammalangkota/" style="color:black">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
-                                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
-                                </svg>
-                            </a>
-                            <a href="https://twitter.com/tugutirtamalang" style="color: black;">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
-                                    <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
-                                </svg>
-                            </a>
-                            <a href="https://www.youtube.com/c/PdamTube" style="color: black;">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-youtube" viewBox="0 0 16 16">
-                                    <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z" />
-                                </svg>
-                            </a>
-                        </div>
+
+                    <div class="col-md-6">
+                        <img src="Assets\img\tim.png" class="ban-img img-fluid" alt="">
                     </div>
                 </div>
             </div>
         </div>
-</footer>
 
+        <svg class="wave"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,192L120,213.3C240,235,480,277,720,261.3C960,245,1200,171,1320,133.3L1440,96L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path></svg>
+      </section>
+
+      <!-- Features -->
+      <section id="features">
+        <div class="container">
+          <h2 class="text-center">Kamu Nanya? Kamu Bertanya Tanya??</h2>
+          <div class="row text-center">
+            <div class="col-md-4 features">
+              <img src="Assets\img\work.png" class="img-fluid" alt="">
+              <h4>Marketing Online</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quae atque quibusdam vero maiores, laborum facere. Deleniti perspiciatis explicabo rerum fugit, voluptatum, animi quia deserunt modi est impedit dicta. Eligendi.</p>
+            </div>
+
+              <div class="col-md-4 features">
+                <img src="Assets\img\package.png" class="img-fluid" alt="">
+                <h4>Hasil Coba</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quae atque quibusdam vero maiores, laborum facere. Deleniti perspiciatis explicabo rerum fugit, voluptatum, animi quia deserunt modi est impedit dicta. Eligendi.</p>
+
+            </div>
+
+            <div class="col-md-4 features">
+              <img src="Assets\img\search.png" class="img-fluid" alt="">
+              <h4>Search</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quae atque quibusdam vero maiores, laborum facere. Deleniti perspiciatis explicabo rerum fugit, voluptatum, animi quia deserunt modi est impedit dicta. Eligendi.</p>
+
+
+              </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="contact">
+        <div class="cotainer">
+          <h2 class="text-center">Hubungi Saya ya ges ya!</h2>
+          <div class="row">
+            <div class="col-md-6">
+              <img src="Assets\img\contact.png" alt="" class="img fluid">
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
 </html>
